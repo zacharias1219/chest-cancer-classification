4a1c30ef89ff4d857e933f14ce81019e295e6493
Richard Abishai <rickoshade1891@gmail.com>
2025-02-18 17:13:23 +0530

    Update model and scores; adjust prediction pipeline to load model from new location

--- src/CNNClassification/pipeline/prediction.py
+++ src/CNNClassification/pipeline/prediction.py
@@ -14,7 +14,6 @@ class PredictionPipeline:
     def predict(self):
         ## load model
         
-        # model = load_model(os.path.join("artifacts","training", "model.h5"))
         model = load_model(os.path.join("model", "model.h5"))
 
         imagename = self.filename
